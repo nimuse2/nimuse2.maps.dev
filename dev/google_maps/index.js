e72549469965d0280578f2c1b5b93ee2d564ab13
Nick Walters <nick.walters@argandsolutions.com>
2023-10-31 21:17:53 +0000

    updated lat long

--- dev/google_maps/index.js
+++ dev/google_maps/index.js
@@ -4,7 +4,8 @@ async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
 
   map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 50.473602050689635, lng: -3.7373521109426804 }, //50.473602050689635, -3.7373521109426804
+    // center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
 }
